7d1d8c819ecd9a28fa5a41e6e5b0ee005d239d25
Hasan Ahmed <hasahmed@ironarchtech.com>
2026-02-08 17:59:22 -0500

    First commit

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if (Input.is_action_pressed("right")):
+		apply_central_force(Vector2(500, 0))
+	if (Input.is_action_pressed("left")):
+		apply_central_force(Vector2(-500, 0))
+	if (Input.is_action_just_pressed("jump")):
+		apply_central_impulse(Vector2(0, -1000))
+		#apply_impulse(Vector2(20, 0))
+		print("right")
+	pass
